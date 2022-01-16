e91d1283b7340bde6af68f6f8dad08080680ac2b
Victor M <vicblanescastro@gmail.com>
2022-01-16 20:24:10 +0100

    Practica 1 ProgConcurrente
    Añadido comentarios y limpiado funciones del codigo

--- Programacion Concurrente/Practica1ProgConcurrente/src/practica1progconcurrente/Practica1ProgConcurrente.java
+++ Programacion Concurrente/Practica1ProgConcurrente/src/practica1progconcurrente/Practica1ProgConcurrente.java
@@ -1,3 +1,13 @@
+/**
+ * Practica1ProgConcurrente.
+ * Practica 1 de programacion concurrente, simula el funcionamiento de un baño 
+ * mixto en un despacho de abogados, hay 6 hombres y mujeres y estos van al baño
+ * 2 veces, en el baño puede caber maximo 3 personas y no pueden haber hombres y
+ * mujeres en el baño al mismo tiempo. Se tiene que simular el acceso al baño 
+ * mediante semaforos.
+ * 
+ * @author Victor Manuel Blanes Castro
+ */
 package practica1progconcurrente;
 
 import java.util.concurrent.Semaphore;
@@ -6,11 +16,14 @@ import java.util.logging.Logger;
 
 public class Practica1ProgConcurrente implements Runnable {
 
-    static final int THREADS = 12;
-    static final int MAX_COUNT = 2;
-    static final int MAX_GENTE_BANYO = 3;
-    static volatile int aforo = 0;
+    static final int THREADS = 12; //Num de hombres + mujeres
+    static final int MAX_COUNT = 2; //Num de veces que van al baño
+    static final int MAX_GENTE_BANYO = 3; //Aforo maximo del baño
+    static volatile int enBaño = 0;
+    
+    //Si hay hombre o mujeres en el baño en el momento, Ninguno si no hay nadie
     static volatile HombreMujer estadoBaño = HombreMujer.Ninguno;
+    
 
     static Semaphore mutex = new Semaphore(1);
     static Semaphore bañoVacio = new Semaphore(MAX_GENTE_BANYO);
@@ -19,54 +32,54 @@ public class Practica1ProgConcurrente implements Runnable {
     HombreMujer hm;
     boolean permit;
 
+    /**
+     * Metodo constructor. Inicializa las variables para crear una objeto de
+     * esta clase, usado inicializar los hilos. La varaible permit se usara mas
+     * adelante para controlar que hombre y mujeres no esten juntos en el baño.
+     *
+     * @param id: Identificador del hilo
+     * @param hm: Usado para distinguir si es un hombre o mujer.
+     */
     public Practica1ProgConcurrente(int id, HombreMujer hm) {
         this.id = id;
         this.hm = hm;
         this.permit = true;
     }
 
+    /**
+     * Metodo run. Metodo que ejecutara cada hilo que se cree, entra y sale del
+     * baño(Seccion critica) MAX_COUNT veces, se añaden esperas para impulsar
+     * mas el intercalado entre hilos.
+     */
     @Override
     public void run() {
-        if (hm == HombreMujer.Hombre) {
-            try {
-                runHombre();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Practica1ProgConcurrente.class.getName()).log(Level.SEVERE, null, ex);
+        try {
+            System.out.printf("Soy %s %d.\n", hm, id);
+            for (int i = 0; i < MAX_COUNT; i++) {
+                System.out.printf("%s %d trabaja [%d/%d].\n", hm, id, (i + 1), MAX_COUNT);
+                entraBaño(hm);
+                Thread.sleep(100); //En el baño
+                salirBaño();
+                Thread.sleep(100); //Trabajando
+
             }
-        } else {
-            try {
-                runMujer();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Practica1ProgConcurrente.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Practica1ProgConcurrente.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    private void runHombre() throws InterruptedException {
-        System.out.printf("Soy %s %d.\n", hm, id);
-        for (int i = 0; i < MAX_COUNT; i++) {
-            System.out.printf("%s %d trabaja [%d/%d].\n", hm, id, (i + 1), MAX_COUNT);
-            entra_baño(hm);
-            Thread.sleep(100); //En el baño
-            salir_baño();
-            Thread.sleep(100); //Trabajando
-
-        }
-    }
-
-    private void runMujer() throws InterruptedException {
-        System.out.printf("Soy %s %d.\n", hm, id);
-        for (int i = 0; i < MAX_COUNT; i++) {
-            System.out.printf("%s %d trabaja [%d/%d].\n", hm, id, (i + 1), MAX_COUNT);
-            entra_baño(hm);
-            Thread.sleep(100); //En el baño
-            salir_baño();
-            Thread.sleep(100); //Trabajando
-
-        }
-    }
-
-    private void entra_baño(HombreMujer hm) throws InterruptedException {
+    /**
+     * Metodo entraBaño. Controla el acceso a la seccion critica, al entrar
+     * aumenta enBaño y pone el estadoBaño a hombre o mujer.
+     *
+     * @param hm
+     * @throws InterruptedException
+     */
+    private void entraBaño(HombreMujer hm) throws InterruptedException {
+        /* Los semaforos solo controlan el aforo y la seccion critica, el bucle 
+        controla que un hombre no acceda cuando hay mujeres. Si es hombre y 
+        hay mujeres hara release de los dos semaforos y seguira dentro del bucle, 
+        bloqueandose otra vez, dejando continuar a otro hilo.*/
         while (permit) {
             permit = false;
             bañoVacio.acquire();
@@ -78,16 +91,23 @@ public class Practica1ProgConcurrente implements Runnable {
             }
         }
         estadoBaño = hm;
-        aforo++;
-        System.out.printf("%s %d esta en el baño [%d/3].\n", hm, id, aforo);
+        enBaño++;
+        System.out.printf("%s %d esta en el baño [%d/3].\n", hm, id, enBaño);
         mutex.release();
     }
-
-    private void salir_baño() throws InterruptedException {
+    
+    /**
+     * Metodo salirBaño. Controla el acceso a la seccion critica, reduce enBaño
+     * y, en el caso de que el baño este vacio, pone el estadoBaño a ninguno
+     * para que puedan entrar hombre y mujeres.
+     * 
+     * @throws InterruptedException 
+     */
+    private void salirBaño() throws InterruptedException {
         mutex.acquire();
-        aforo--;
-        System.out.printf("%s %d sale en el baño [%d/3].\n", hm, id, aforo);
-        if (aforo == 0) {
+        enBaño--;
+        System.out.printf("%s %d sale en el baño [%d/3].\n", hm, id, enBaño);
+        if (enBaño == 0) {
             System.out.println("****BAÑO VACIO****");
             estadoBaño = HombreMujer.Ninguno;
         }
@@ -96,12 +116,18 @@ public class Practica1ProgConcurrente implements Runnable {
         mutex.release();
     }
 
+    /**
+     * Main.
+     * Lanza un numero THREADS de hilos y espera a que terminen.
+     * @param args
+     * @throws InterruptedException 
+     */
     public static void main(String[] args) throws InterruptedException {
         Thread[] threads = new Thread[THREADS];
         int i;
 
         for (i = 0; i < THREADS; i++) {
-            if (i < THREADS / 2) {
+            if (i % 2 == 0) {
                 threads[i] = new Thread(new Practica1ProgConcurrente(i, HombreMujer.Hombre));
             } else {
                 threads[i] = new Thread(new Practica1ProgConcurrente(i, HombreMujer.Mujer));
